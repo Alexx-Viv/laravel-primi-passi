d14666b28da398e2028db5988615b1453ecfe0f8
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-30 16:28:40 +0200

    Add Blog and News pages with relative route

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,15 +4,15 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+  <title>News</title>
 </head>
 <body>
   <header style="height:60px; display:flex; justify-content:end; background-color:gray; padding:10px;">
   <nav>
+  <a href="/">Home</a>
   <a href="blog">Blog</a>
-  <a href="news">News</a>
   </nav>
 </header>
-  <h1>Hello World!</h1>
+  <h1>News</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
